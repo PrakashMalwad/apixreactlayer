816c1f92cff1890f6df95b85af6f9ff0d87ce132
Prakash Vijayan Malwad <prakashmalwad8778999020@gmail.com>
2025-03-05 12:46:02 +0530

    Create api.js

---
+++ src/components/api.js
@@ -0,0 +1,26 @@
+// api.js
+import { getAuthHeaders } from './authUtils';
+
+export const fetchData = async (url, auth) => {
+  const headers = getAuthHeaders(auth);
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
